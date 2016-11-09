f2dd3bc435d43fe3c311d31bed5d78f22c47e5ab
dhillonsh <dhillonsh@gmail.com>
2016-11-08 18:12:47 -0800

    Update client_secrets.py

--- secrets/client_secrets.py
+++ secrets/client_secrets.py
@@ -3,11 +3,3 @@
 
 author = "Harpreet Dhillon"                               # As it appears in Canvas
 repo = "https://github.com/dhillonsh/proj7-gcal.git"  # Replace with yours
-server_main = "flask_main"                              # flask_main.py is my main file
-
-# The database we will use within the MongoDB instance
-# for the Memos of Mongo application 
-db = "calendar"
-# The MongoDB user who will have access to read and write this database
-db_user = "cal_user"
-db_user_pw = "tftttgfdtfdmga"  # Can you identify the poem? 

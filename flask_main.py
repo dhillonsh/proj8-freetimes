e31c3c99b6c997542f08f7d739352d07cf63772e
dhillonsh <dhillonsh@gmail.com>
2016-11-08 19:07:57 -0800

    Update flask_main.py

--- flask_main.py
+++ flask_main.py
@@ -97,6 +97,7 @@ def choose():
 
 @app.route('/selectcalendars', methods=['POST'])
 def selectcalendars():
+  print("--- In select calendars")
   print(request.form.get('calendarList'))
   return render_template('index.html')
 ####

c617e821d03a66094f3b3a9a2d5288b361c502d9
dhillonsh <dhillonsh@gmail.com>
2016-11-16 21:06:22 -0800

    Update flask_main.py

--- flask_main.py
+++ flask_main.py
@@ -256,14 +256,14 @@ def agenda(startDay, endDay, startTime, endTime, busyList):
     event_end = arrow.get(event['end'])
     
     if cur_time < event_start:
-      while cur_time < event_start:
+      while cur_time < event_start.replace(hour=begin_time.hour,minute=begin_time.minute):
         if cur_time < cur_time.replace(hour=end_time.hour, minute=end_time.minute):
-          fullAgenda.append({'summary': 'Available', 'start': cur_time.isoformat().format("ddd MM/DD/YYYY HH:mm"), 'end': event_start.isoformat(), 'formattedDate': cur_time.format("ddd MM/DD/YYYY HH:mm") + ' - ' + event_start.format("ddd MM/DD/YYYY HH:mm")})
+          fullAgenda.append({'summary': 'Available', 'start': cur_time.isoformat().format("ddd MM/DD/YYYY HH:mm"), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat(), 'formattedDate': cur_time.format("ddd MM/DD/YYYY HH:mm") + ' - ' + cur_time.replace(hour=end_time.hour, minute=end_time.minute).format("ddd MM/DD/YYYY HH:mm")})
         print("Cur_time: " + cur_time.isoformat())
         print("Event_Start: " + event_start.isoformat())
         print('Time section here for [' + event['summary'] + ']')
         cur_time = cur_time.replace(hour=begin_time.hour, minute=begin_time.minute,days=+1)
-        
+      fullAgenda.append({'summary': 'Available', 'start': cur_time.isoformat().format("ddd MM/DD/YYYY HH:mm"), 'end': event_start.isoformat(), 'formattedDate': cur_time.format("ddd MM/DD/YYYY HH:mm") + ' - ' + event_start.format("ddd MM/DD/YYYY HH:mm")})        
     cur_time = event_end
     fullAgenda.append(event)
 

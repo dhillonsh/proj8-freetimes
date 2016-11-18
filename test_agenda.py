482c4d1344dd58695a3b37eab71e16f28951a7f7
dhillonsh <dhillonsh@gmail.com>
2016-11-17 16:37:42 -0800

    Update test_agenda.py

--- test_agenda.py
+++ test_agenda.py
@@ -2,5 +2,12 @@ from agenda import *
 #import arrow
 
 
-def test_empty():
-  print(agenda("2016-11-17","2016-11-17","2016-11-17T08:00:00:00", "2016-11-17T17:00:00:00", []))
+def test_noEvents():
+  singleDay = agenda("2016-11-17","2016-11-17","2016-11-17T08:00:00:00", "2016-11-17T17:00:00:00", [])
+  assert len(singleDay) == 1
+  assert singleDay[0]['start'].format('YYYY-MM-DD HH:mm') == "2016-11-17 08:00"
+  assert singleDay[0]['end'].format('YYYY-MM-DD HH:MM') == "2016-11-17 17:00"
+  
+  multipleDays = agenda("2016-11-17", "2016-11-18","2016-11-17T08:00:00:00","2016-11-18T17:00:00:00", [])
+  assert len(multipleDays) == 2
+  print(multipleDays)

d03fbbea9d67ff8539c5e63dbeaa3f9aff5028b5
dhillonsh <dhillonsh@gmail.com>
2016-11-17 18:59:26 -0800

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -15,11 +15,11 @@ def agenda(startDay, endDay, startTime, endTime, busyList):
     if cur_time < event_start:
       while cur_time < event_start.replace(hour=begin_time.hour,minute=begin_time.minute):
         if cur_time < cur_time.replace(hour=end_time.hour, minute=end_time.minute):
-          toAppend = {'summary': 'Available', 'start': cur_time.format("ddd MM/DD/YYYY HH:mm"), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).format("ddd MM/DD/YYYY HH:mm")}
+          toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat()}
           toAppend['formattedDate'] = formatDates(cur_time.isoformat(), cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat())
           fullAgenda.append(toAppend)
         cur_time = cur_time.replace(hour=begin_time.hour, minute=begin_time.minute,days=+1)
-      toAppend = {'summary': 'Available', 'start': cur_time.format("ddd MM/DD/YYYY HH:mm"), 'end': event_start.format("ddd MM/DD/YYYY HH:mm")}
+      toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': event_start.isoformat()}
       toAppend['formattedDate'] = formatDates(cur_time.isoformat(), event_start.isoformat())
       fullAgenda.append(toAppend)
     cur_time = event_end
@@ -28,7 +28,7 @@ def agenda(startDay, endDay, startTime, endTime, busyList):
   #Fill in the days after the last event as available
   while cur_time < end_date:
     if cur_time < cur_time.replace(hour=end_time.hour, minute=end_time.minute):
-      toAppend = {'summary': 'Available', 'start': cur_time.format("ddd MM/DD/YYYY HH:mm"), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).format("ddd MM/DD/YYYY HH:mm")}
+      toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat()}
       toAppend = formatDates(cur_time.isoformat(), cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat())
       fullAgenda.append(toAppend)
     cur_time = cur_time.replace(hour=begin_time.hour, minute=begin_time.minute,days=+1)

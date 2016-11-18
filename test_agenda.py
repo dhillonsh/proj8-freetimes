9847a7a82b06cc8c3c45ec2e6bb32a0512330e95
dhillonsh <dhillonsh@gmail.com>
2016-11-17 16:48:19 -0800

    Update test_agenda.py

--- test_agenda.py
+++ test_agenda.py
@@ -22,7 +22,7 @@ def test_singleEvent():
   
   #Event at the start of the day and time
   startDayEvent = agenda("2016-11-17","2016-11-17","2016-11-17T08:00:00:00", "2016-11-17T17:00:00:00", busyList)
-  assert len(singleDay) == 2
+  assert len(startDayEvent) == 2
   #assert arrow.get(singleDay[0]['start']).format('YYYY-MM-DD HH:mm') == "2016-11-17 08:00"
   #assert arrow.get(singleDay[0]['end']).format('YYYY-MM-DD HH:mm') == "2016-11-17 17:00"
   print(startDayEvent)

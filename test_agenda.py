b229010c92226c19d660541e05ae574b526442af
dhillonsh <dhillonsh@gmail.com>
2016-11-17 16:43:33 -0800

    Update test_agenda.py

--- test_agenda.py
+++ test_agenda.py
@@ -10,4 +10,7 @@ def test_noEvents():
   
   multipleDays = agenda("2016-11-17", "2016-11-18","2016-11-17T08:00:00:00","2016-11-18T17:00:00:00", [])
   assert len(multipleDays) == 2
-  print(multipleDays)
+  assert arrow.get(multipleDays[0]['start']).format('YYYY-MM-DD HH:mm') == "2016-11-17 08:00"
+  assert arrow.get(multipleDays[0]['end']).format('YYYY-MM-DD HH:mm') == "2016-11-17 17:00"
+  assert arrow.get(multipleDays[1]['start']).format('YYYY-MM-DD HH:mm') == "2016-11-18 08:00"
+  assert arrow.get(multipleDays[1]['end']).format('YYYY-MM-DD HH:mm') == "2016-11-18 17:00"

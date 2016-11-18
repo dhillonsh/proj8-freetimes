27d0c3b2f5c0756ef7010bcad39c581e8cb08792
dhillonsh <dhillonsh@gmail.com>
2016-11-17 17:43:03 -0800

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -15,16 +15,22 @@ def agenda(startDay, endDay, startTime, endTime, busyList):
     if cur_time < event_start:
       while cur_time < event_start.replace(hour=begin_time.hour,minute=begin_time.minute):
         if cur_time < cur_time.replace(hour=end_time.hour, minute=end_time.minute):
-          fullAgenda.append({'summary': 'Available', 'formattedDate': formatDates(cur_time.isoformat(), cur_time.replace(hour=end_time.hour, minute=end_time.minute))})
+          toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute)}
+          toAppend['formattedDate'] = formatDates(toAppend['start'], toAppend['end'])
+          fullAgenda.append(toAppend)
         cur_time = cur_time.replace(hour=begin_time.hour, minute=begin_time.minute,days=+1)
-      fullAgenda.append({'summary': 'Available', 'formattedDate': formatDates(cur_time.isoformat(), event_start.isoformat())})
+      toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': event_start.isoformat()}
+      toAppend['formattedDate'] = formatDates(toAppend['start'], toAppend['end'])
+      fullAgenda.append(toAppend)
     cur_time = event_end
     fullAgenda.append(event)
 
   #Fill in the days after the last event as available
   while cur_time < end_date:
     if cur_time < cur_time.replace(hour=end_time.hour, minute=end_time.minute):
-      fullAgenda.append({'summary': 'Available', 'formattedDate': formatDates(cur_time.isoformat(), cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat())})
+      toAppend = {'summary': 'Available', 'start': cur_time.isoformat(), 'end': cur_time.replace(hour=end_time.hour, minute=end_time.minute).isoformat()}
+      toAppend = formatDates(toAppend['start'], toAppend['end'])
+      fullAgenda.append(toAppend)
     cur_time = cur_time.replace(hour=begin_time.hour, minute=begin_time.minute,days=+1)
       
   return fullAgenda
